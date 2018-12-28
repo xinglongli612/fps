84e6fe6022d024f7b60edeca9268ba145c1b0f49
lxl <lilxinglong@126.com>
2018-12-28 21:46:27 +0800

    fps hello

---
+++ hello/Assets/HelloUnity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloUnity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("HelloUnity");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("HelloUnity");
+	}
+}
