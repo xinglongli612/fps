d5fb5383a24c38f0fdf62d052e4c26907535ab14
lxl <lilxinglong@126.com>
2019-01-03 09:22:49 +0800

    FPSInput

---
+++ FPS/Assets/FPSInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour {
+	public float speed = 6.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+		transform.Translate (deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+	}
+}
