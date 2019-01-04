0a2cacc42830a9969baf2ebd81aea9a718a78c21
lxl <lilxinglong@126.com>
2019-01-04 09:12:31 +0800

    add RayShooter

---
+++ FPS/Assets/RayShooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour {
+	private Camera _camera;
+	// Use this for initialization
+	void Start () {
+		_camera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+			Ray ray = _camera.ScreenPointToRay (point);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) {
+				Debug.Log ("Hit " + hit.point);
+			}
+		}
+	}
+}
