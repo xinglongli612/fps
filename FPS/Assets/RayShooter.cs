92f7f98de3020e274cbb1c60255ca498046fe67e
lxl <lilxinglong@126.com>
2019-01-04 23:38:07 +0800

    RayHit

--- FPS/Assets/RayShooter.cs
+++ FPS/Assets/RayShooter.cs
@@ -7,6 +7,8 @@ public class RayShooter : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		_camera = GetComponent<Camera> ();
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
@@ -16,8 +18,23 @@ public class RayShooter : MonoBehaviour {
 			Ray ray = _camera.ScreenPointToRay (point);
 			RaycastHit hit;
 			if (Physics.Raycast (ray, out hit)) {
-				Debug.Log ("Hit " + hit.point);
+				StartCoroutine (SphereIndicator(hit.point));
+				//Debug.Log(hit.point);
 			}
 		}
+	}
+	private IEnumerator SphereIndicator(Vector3 pos){
+		GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		sphere.transform.position = pos;
+		yield return new WaitForSeconds (1);
+
+		Destroy (sphere);
+	}
+
+	void OnGUI(){
+		int size = 12;
+		float posX = _camera.pixelWidth / 2 - size / 4;
+		float posY = _camera.pixelHeight / 2 - size / 2;
+		GUI.Label (new Rect (posX, posY, size, size), "*");
 	}
 }
