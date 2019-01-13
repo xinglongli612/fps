1ccf9b5a741a85e7670f19da4a52333fe520a0e6
lxl <lilxinglong@126.com>
2019-01-14 01:22:11 +0800

    MemoryCard

---
+++ MemoryCard/Assets/UIButton.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+
+	[SerializeField] private GameObject targetObject;
+	[SerializeField] private string targetMessage;
+
+	public Color highlightColor = Color.cyan;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnMouseEnter () {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+		if (sprite != null) {
+			sprite.color = highlightColor;
+		}
+	}
+
+	public void OnMouseExit () {
+		SpriteRenderer sprite = GetComponent<SpriteRenderer> ();
+		if (sprite != null) {
+			sprite.color = Color.white;
+		}
+	}
+
+	public void OnMouseDown () {
+		transform.localScale = new Vector3 (1.1f,1.1f,1.1f);
+	}
+
+	public void OnMouseUp () {
+		transform.localScale = Vector3.one;
+		if (targetObject != null) {
+			targetObject.SendMessage (targetMessage);
+		}
+	}
+}
