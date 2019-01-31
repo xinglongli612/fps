f4986b9224effb724f8a01b8a22a718a5f22571f
lxl <lilxinglong@126.com>
2019-01-31 12:20:10 +0800

    weather

---
+++ ch09_weather/Assets/Managers.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(WeatherManager))]
+public class Managers : MonoBehaviour {
+
+	public static WeatherManager Weather{ get; private set;}
+	private List<IGameManager> _startSequence;
+
+	void Awake(){
+		Weather = GetComponent<WeatherManager> ();
+
+		_startSequence = new List<IGameManager> ();
+		_startSequence.Add (Weather);
+
+		StartCoroutine (StartupManagers());
+	}
+
+	private IEnumerator StartupManagers (){
+		NetworkService network = new NetworkService ();
+		foreach (IGameManager manager in _startSequence) {
+			manager.Startup (network);
+		}
+		yield return null;
+	
+		int numModules = _startSequence.Count;
+		int numReady = 0;
+
+		while (numReady < numModules) {
+			int lastReady = numReady;
+			numReady = 0;
+			foreach (IGameManager manager in _startSequence) {
+				if (manager.status == ManagerStatus.Started) {
+					numReady++;
+				}
+			}
+
+			if (numReady > lastReady) {	
+				Debug.Log ("Progress: " + numReady + "/" + numModules);
+				yield return null;
+			}
+
+			Debug.Log ("All managers started up");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ch09_weather/Assets/WeatherManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherManager : MonoBehaviour, IGameManager {
+	public ManagerStatus status { get; private set;}
+
+	private NetworkService _network;
+
+	public void Startup(NetworkService service){
+		Debug.Log ("Weather manager starting...");
+		_network = service;
+		status = ManagerStatus.Started;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
