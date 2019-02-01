83e22748a2544724122a3b3ccf404459c9235b5f
lxl <lilxinglong@126.com>
2019-02-01 17:08:54 +0800

    music

---
+++ ch10_music/Assets/Managers.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioManager))]
+public class Managers : MonoBehaviour {
+
+	public static AudioManager Audio{ get; private set;}
+	private List<IGameManager> _startSequence;
+
+	void Awake(){
+		Audio = GetComponent<AudioManager> ();
+		_startSequence = new List<IGameManager> ();
+		_startSequence.Add (Audio);
+		StartCoroutine (StartupManagers());
+	}
+
+	private IEnumerator StartupManagers (){
+		NetworkService network = new NetworkService ();
+		foreach (IGameManager manager in _startSequence) {
+			manager.Startup (network);
+		}
+		yield return null;
+	
+		int numModules = _startSequence.Count;
+		int numReady = 0;
+
+		while (numReady < numModules) {
+			int lastReady = numReady;
+			numReady = 0;
+			foreach (IGameManager manager in _startSequence) {
+				if (manager.status == ManagerStatus.Started) {
+					numReady++;
+				}
+			}
+
+			if (numReady > lastReady) {	
+				Debug.Log ("Progress: " + numReady + "/" + numModules);
+				yield return null;
+			}
+
+			Debug.Log ("All managers started up");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
