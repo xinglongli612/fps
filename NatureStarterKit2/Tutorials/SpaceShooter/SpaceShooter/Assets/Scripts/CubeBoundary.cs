b8c7a565e8baf0c31c1950b27ae42a82b0aa15aa
lxl <xinglongli521@163.com>
2019-04-11 00:08:40 +0800

    creating-hazards

---
+++ NatureStarterKit2/Tutorials/SpaceShooter/SpaceShooter/Assets/Scripts/CubeBoundary.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBoundary : MonoBehaviour {
+	void OnTriggerExit(Collider other){
+		Destroy (other.gameObject);
+	}
+}
---
+++ NatureStarterKit2/Tutorials/SpaceShooter/SpaceShooter/Assets/Scripts/DestoryByContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryByContact : MonoBehaviour {
+	void OnTriggerEnter(Collider other){
+		//Debug.Log (other.name);
+		if (other.tag == "Boundary") {
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
