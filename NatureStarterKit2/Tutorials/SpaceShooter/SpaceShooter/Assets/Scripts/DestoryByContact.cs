3e4574b938569441420dc0738bba06758cd9eb94
lxl <lilxinglong@126.com>
2019-04-11 21:26:21 +0800

    update

--- NatureStarterKit2/Tutorials/SpaceShooter/SpaceShooter/Assets/Scripts/DestoryByContact.cs
+++ NatureStarterKit2/Tutorials/SpaceShooter/SpaceShooter/Assets/Scripts/DestoryByContact.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestoryByContact : MonoBehaviour {
+	public GameObject explode;
+	public GameObject playerExplode;
 	void OnTriggerEnter(Collider other){
-		//Debug.Log (other.name);
 		if (other.tag == "Boundary") {
 			return;
+		}
+		Instantiate (explode, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplode, other.transform.position, other.transform.rotation);
 		}
 		Destroy (other.gameObject);
 		Destroy (gameObject);
